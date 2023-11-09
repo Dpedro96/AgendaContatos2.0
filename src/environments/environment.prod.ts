df0adaf1554ef94eb550660ea0948de8af1a31a5
Dpedro96 <pedrohferreira300402@gmail.com>
2023-11-09 10:46:04 -0300

    'first'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-   //inserir aqui sua chave
+    apiKey: "AIzaSyA5E2aY0OPcfwYbnsuxN4m6jCCIUV2td28",
+    authDomain: "agendacontatos-e24e4.firebaseapp.com",
+    projectId: "agendacontatos-e24e4",
+    storageBucket: "agendacontatos-e24e4.appspot.com",
+    messagingSenderId: "251756414535",
+    appId: "1:251756414535:web:9a3e6771150bbd9ebe5362",
+    measurementId: "G-KZE2G7S12K"
   }
 };
